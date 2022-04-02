1ac0b26c73fc917afa3125ada01946b79e932c95
Ovidiu Negulescu <ovinegules@gmail.com>
2022-04-02 16:20:48 +0000

    Add snapshot

---
+++ greenery/snapshots/orders.sql
@@ -0,0 +1,15 @@
+{% snapshot orders_snapshot %}
+
+  {{
+    config(
+      target_schema='snapshots',
+      unique_key='id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+  }}
+
+  SELECT * FROM {{ source('orders') }}
+
+{% endsnapshot %}
